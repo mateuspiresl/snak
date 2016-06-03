7f75165dfdc3ca87f0c508787dc6f62b1a51ada5
Mateus Pires <mateusplpl@gmail.com>
2016-06-03 18:00:31 -0300

    Program passa a ser derivado de JFrame; Adicionado função para botão voltar da tela de criação; Adicionada a tela de espera

--- src/com/forbait/games/snake/Program.java
+++ src/com/forbait/games/snake/Program.java
@@ -2,69 +2,62 @@ package com.forbait.games.snake;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.forbait.games.snake.server.Server;
+import com.forbait.games.snake.ui.ClientsConnectionListener;
 import com.forbait.games.snake.ui.CreatePanel;
 import com.forbait.games.snake.ui.StartPanel;
+import com.forbait.games.snake.ui.WaitingClientsPanel;
 
-public class Program implements ActionListener { //ItemListener {
+public class Program extends JFrame implements ActionListener { //ItemListener {
 
-	private static final Program INSTANCE = new Program();
-	
-	private JPanel cards;
+	private static Program INSTANCE = null;
 	public final static String PANEL_START = "start";
 	public final static String PANEL_CREATE = "create";
+	public final static String PANEL_WAITING = "waiting";
 	
-	public static Program get() {
-		return INSTANCE;
+	private JPanel cards;
+	private ClientsConnectionListener connectionListener;
+	
+	private Program()
+	{
+		super("Snak");
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Program.INSTANCE = this;
+		
+		WaitingClientsPanel clientsPanel = new WaitingClientsPanel();
+		this.connectionListener = clientsPanel;
+		
+		this.cards = new JPanel(new CardLayout());
+		this.cards.add(new StartPanel(this), PANEL_START);
+		this.cards.add(new CreatePanel(), PANEL_CREATE);
+		this.cards.add(clientsPanel, PANEL_WAITING);
+		
+		super.add(this.cards, BorderLayout.CENTER);
+		super.pack();
+		super.setVisible(true);
 	}
 	
-	public void addComponentToPane(Container pane)
-	{	 
-		//Create the "cards".
-		JPanel card1 = new StartPanel(this);
-		JPanel card2 = new CreatePanel();
-		
-		//Create the panel that contains the "cards".
-		cards = new JPanel(new CardLayout());
-		cards.add(card1, PANEL_START);
-		cards.add(card2, PANEL_CREATE);
-		
-		pane.add(cards, BorderLayout.CENTER);
+	public static Program get()
+	{
+		if (Program.INSTANCE == null) new Program();
+		return Program.INSTANCE;
 	}
 	 
 	public void changePanel(String panelName)
 	{
-		CardLayout cl = (CardLayout) cards.getLayout();
-		cl = (CardLayout) cards.getLayout();
-		cl.show(cards, panelName);
-	}
-	 
-	/**
-	 * Create the GUI and show it.  For thread safety,
-	 * this method should be invoked from the
-	 * event dispatch thread.
-	 */
-	private static void createAndShowGUI() {
-		//Create and set up the window.
-		JFrame frame = new JFrame("Snak");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
-		//Create and set up the content pane.
-		Program prog = new Program();
-		prog.addComponentToPane(frame.getContentPane());
-		 
-		//Display the window.
-		frame.pack();
-		frame.setVisible(true);
+		System.out.println(this.cards);
+		CardLayout cl = (CardLayout) this.cards.getLayout();
+		cl.show(this.cards, panelName);
 	}
 
 	@Override
@@ -77,9 +70,12 @@ public class Program implements ActionListener { //ItemListener {
 			break;
 			
 		case StartPanel.BUTTON_CONNECT:
+			break;
 			
 		case StartPanel.BUTTON_EXIT:
+			break;
 			
+		case CreatePanel.BUTTON_BACK:
 		default:
 			changePanel(PANEL_START);
 			break;
@@ -88,11 +84,25 @@ public class Program implements ActionListener { //ItemListener {
 	
 	public void createGame(int numPlayers, int width, int height)
 	{
+		this.connectionListener.setClientsCounter(numPlayers, 1);
+		changePanel(PANEL_WAITING);
+		
+		if (numPlayers > 1) try
+		{
+			Server server = new Server(8001, numPlayers - 1, this.connectionListener);
+			server.waitClients();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			
+			// TODO
+		}
+		
 		new Game(numPlayers, width, height);
 	}
 	
-	public static void main(String[] args) {
-		/* Use an appropriate Look and Feel */
+	public static void main(String[] args)
+	{
 		try {
 			//UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
@@ -101,14 +111,14 @@ public class Program implements ActionListener { //ItemListener {
 			ex.printStackTrace();
 		}
 		
-		/* Turn off metal's use of bold fonts */
+		// Turn off metal's use of bold fonts
 		UIManager.put("swing.boldMetal", Boolean.FALSE);
 		 
-		//Schedule a job for the event dispatch thread:
-		//creating and showing this application's GUI.
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				createAndShowGUI();
+				Program.get();
 			}
 		});
 	}

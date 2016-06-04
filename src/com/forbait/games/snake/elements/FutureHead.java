53994cbe15f272e06f39b7be807c17ff6661b41e
Mateus Pires <mateusplpl@gmail.com>
2016-06-03 22:58:19 -0300

    Creates future head position object

---
+++ src/com/forbait/games/snake/elements/FutureHead.java
@@ -0,0 +1,17 @@
+package com.forbait.games.snake.elements;
+
+import java.awt.Color;
+
+public class FutureHead {
+
+	private Snake snake;
+
+	public FutureHead(Snake snake) {
+		this.snake = snake;
+	}
+	
+	public Snake getSnake() {
+		return this.snake;
+	}
+	
+}

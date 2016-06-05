92155efea780eed530fc7fdae740640ca21324f9
Mateus Pires <mateusplpl@gmail.com>
2016-06-05 08:33:31 -0300

    Creates test to single player

---
+++ src/com/forbait/games/snake/test/SinglePlayer.java
@@ -0,0 +1,13 @@
+package com.forbait.games.snake.test;
+
+import com.forbait.games.snake.Dimension;
+import com.forbait.games.snake.Game;
+
+public class SinglePlayer {
+
+	public static void main(String[] args)
+	{
+		new Game(1, new Dimension(30, 30), null);
+	}
+	
+}
